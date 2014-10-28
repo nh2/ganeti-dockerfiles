6c313fb18405b9bb016ae77eda1538b08f443fd3
Niklas Hambuechen <niklash@google.com>
2014-10-28 13:43:37 +0100

    Initial Dockerfiles (ubuntu1404 and debian7)

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+# Ubuntu 14.04
+for d in ganeti-ubuntu1404-{deps,deps-syshs,code-syshs}; do
+  docker build -t nh2docker/$d $d
+done
+
+# Debian 7 (Wheezy)
+for d in ganeti-debian7-{deps,deps-syshs,code-syshs}; do
+  docker build -t nh2docker/$d $d
+done
---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+# Ubuntu 14.04
+for i in ganeti-ubuntu1404-{deps,deps-syshs,code-syshs}; do
+  docker push nh2docker/$i
+done
+
+# Debian 7 (Wheezy)
+for i in ganeti-debian7-{deps,deps-syshs,code-syshs}; do
+  docker push nh2docker/$i
+done
