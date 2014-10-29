05c712ec763042574752f54884b465aaf2967b3b
Niklas Hambuechen <niklash@google.com>
2014-10-29 15:41:53 +0100

    Add ubuntu1204 container

--- build.sh
+++ build.sh
@@ -7,6 +7,11 @@ for d in ganeti-ubuntu1404-{deps,deps-syshs,code-syshs}; do
   docker build -t nh2docker/$d $d
 done
 
+# Ubuntu 12.04
+for d in ganeti-ubuntu1204-{deps,deps-syshs,code-syshs}; do
+  docker build -t nh2docker/$d $d
+done
+
 # Debian 7 (Wheezy)
 for d in ganeti-debian7-{deps,deps-syshs,code-syshs}; do
   docker build -t nh2docker/$d $d
--- push.sh
+++ push.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Ubuntu 12.04
+for i in ganeti-ubuntu1204-{deps,deps-syshs,code-syshs}; do
+  docker push nh2docker/$i
+done
+
 # Ubuntu 14.04
 for i in ganeti-ubuntu1404-{deps,deps-syshs,code-syshs}; do
   docker push nh2docker/$i
